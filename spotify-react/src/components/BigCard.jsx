665fcb94a1e280fbfa2c6645105441692f6a5ce8
FarhanaRafi <118438210+FarhanaRafi@users.noreply.github.com>
2023-02-17 17:03:40 +0100

    done

--- spotify-react/src/components/BigCard.jsx
+++ spotify-react/src/components/BigCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Card } from "react-bootstrap";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getSelectedSongAsync } from "../redux/actions";
 import { AiOutlineHeart } from "react-icons/ai";
 import { useState } from "react";
@@ -8,6 +8,15 @@ import { useState } from "react";
 const BigCard = ({ card }) => {
   const dispatch = useDispatch();
   const [active, setActive] = useState(false);
+  const getFavRedux = useSelector((state) => state.likes.likedSongs);
+
+  const isfav = (id) => {
+    if (getFavRedux.includes(id)) {
+      return true;
+    } else {
+      return false;
+    }
+  };
   return (
     <Card
       style={{ height: "200px", width: "200px" }}
@@ -18,13 +27,24 @@ const BigCard = ({ card }) => {
     >
       <Card.Img variant="top" src={card.album.cover_medium} />
       <Card.Body className="background">
-        <AiOutlineHeart
-          color={active ? "red" : "black"}
-          onClick={() => {
-            setActive(true);
-            dispatch(card.id);
-          }}
-        />
+        {isfav ? (
+          <AiOutlineHeart
+            color={active ? "red" : "black"}
+            onClick={() => {
+              setActive(true);
+              dispatch(card.id);
+            }}
+          />
+        ) : (
+          <AiOutlineHeart
+            color={active ? "red" : "black"}
+            onClick={() => {
+              setActive(false);
+              dispatch(card.id);
+            }}
+          />
+        )}
+
         <Card.Title className="title">{card.album.title}</Card.Title>
         <Card.Text className="artist-name">
           <small>{card.artist.name}</small>
